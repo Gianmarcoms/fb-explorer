7d0368aa06e4fd1a048881cc690b8456337ce296
GM <97840371+Gianmarcoms@users.noreply.github.com>
2026-02-15 19:26:46 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FlowBasedExplorer from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FlowBasedExplorer />
+  </React.StrictMode>,
+)
