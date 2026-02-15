036c5967979f822b7af6badda6b2b796a509d780
GM <97840371+Gianmarcoms@users.noreply.github.com>
2026-02-15 19:25:39 +0100

    Delete fb-explorer directory

--- fb-explorer/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FlowBasedExplorer from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FlowBasedExplorer />
-  </React.StrictMode>,
-)
